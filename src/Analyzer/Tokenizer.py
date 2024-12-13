ea4447da265c3df63ac7a85045e49960ec58e7d8
shebov3 <shehabtarik425@yahoo.com>
2024-12-13 20:58:04 +0200

    v1.41

--- src/Analyzer/Tokenizer.py
+++ src/Analyzer/Tokenizer.py
@@ -2,12 +2,25 @@ import spacy
 
 nlp = spacy.load('ja_core_news_sm')
 
-def tokenize(text: str) -> list[str]:
+def tokenize(text: str) -> dict[str, int]:
     """
         input: 日本語を一番好きな言語
         output: {'日本': 1, '一番': 1, '好き': 1, '言語': 1}
     """
     doc = nlp(text)
-    tokens = [token.lemma_ for token in doc if not token.is_punct and not token.is_stop and not token.text == r"\u3000" and not token.like_num]
+    
+    
+    tokens = [
+        token.lemma_ for token in doc
+        if not token.is_punct  
+        and not token.is_stop  
+        and not token.text == "\u3000"  
+        and not token.like_num  
+        and len(token.text.strip()) > 1  
+    ]
+    
+    token_counts = {}
+    for token in tokens:
+        token_counts[token] = token_counts.get(token, 0) + 1
 
-    return tokens
+    return token_counts
